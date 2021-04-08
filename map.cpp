675f758c909d980ceca0078cd67f2153d4ddd3f3
unknown <teddy_chou@outlook.com>
2021-04-08 15:30:29 +0800

    the ver of readin

--- map.cpp
+++ map.cpp
@@ -1,5 +1,5 @@
 #include "map.h"
-#include<fstream>
+#include <fstream>
 using namespace std;
 Map::Map(QString fileDir)
 {
@@ -8,7 +8,7 @@ Map::Map(QString fileDir)
     //1st read the total num and initiate
 
     fstream map_file;
-    string 
+    
     map_file.open("fileDir",ios::in);
     if (!map_file)
     {
@@ -16,10 +16,27 @@ Map::Map(QString fileDir)
     }
     else{
         cout<<"File found!!"<<endl;
+        //the first line should be eaten in advance
+        
+        int tot,id,x,y,cnt;
+        QString name[999];
+        map_file>>tot;
+        point = (Point*)malloc(tot*sizeof (Point));
+        for (int i = 0; i < tot; i++)
+        {
+            map_file>>id>>x>>y>>cnt;
+            for (int j = 0; j < cnt; j++)
+            {
+                map_file>>name[j];
+            }
+
+            point[i] = Point(id,x,y,cnt,name);
+            
+        }
         
     }
     
-    point = (Point*)malloc(dotNum*sizeof (Point));
+    
 
     //2nd read each point and put into the point vec
 }
--- point.cpp
+++ point.cpp
@@ -4,6 +4,11 @@ Point::Point(int _id,int _x,int _y,int cnt,QString* list){
     id = _id;
     x = _x;
     y = _y;
+    for (int i = 0; i < cnt; i++)
+    {
+        name[i] = list[i];
+    }
+    
 
 }
 
